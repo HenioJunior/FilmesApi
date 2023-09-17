60e1e2e5409b75ee42e438dbfda6d2dbb0b3e9cb
Henio Junior <henioajunior@gmail.com>
2023-09-17 18:51:44 -0300

    Adicionado Cinema

---
+++ FilmesApi/Controllers/CinemaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoMapper;
+using FilmesApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+
+[ApiController]
+[Route("[Controller]")]
+public class CinemaController: ControllerBase
+{
+	private FilmeContext _context;
+	private IMapper _mapper;
+
+	public CinemaController(FilmeContext context, IMapper mapper)
+	{
+		_context = context;
+		_mapper = mapper;
+
+	}
+
+		
+}
+
