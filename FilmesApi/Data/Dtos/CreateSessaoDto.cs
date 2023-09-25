6e9e9de20663d95aa5e5e4ccdc702ee60e3fc886
Henio Junior <henioajunior@gmail.com>
2023-09-25 09:59:34 -0300

    Criado o relacionamento Filme e Sessao

--- FilmesApi/Data/Dtos/CreateSessaoDto.cs
+++ FilmesApi/Data/Dtos/CreateSessaoDto.cs
@@ -2,5 +2,5 @@ namespace FilmesApi.Data.Dtos;
 
 public class CreateSessaoDto
 {
-    
+    public int FilmeId { get; set; }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -19,5 +19,7 @@ public class Filme
     [Required]
     [Range(70, 600, ErrorMessage ="A duração deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
+
+    public ICollection<Sessao> Sessoes;
 }
 
